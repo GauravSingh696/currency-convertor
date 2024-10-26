0c2c369f498f1873ee27d732302405f2d2b4544f
Gaurav Singh <161499206+GauravSingh696@users.noreply.github.com>
2024-10-26 10:25:46 +0530

    script.js

--- script.js
+++ script.js
@@ -9,21 +9,18 @@ const update_rate = async() => {
     let amount = document.querySelector('.amount');
     let amt_value = amount.value;
 
-    if(amt_value === "" && amt_value < 1) {
-        amount.value= 1;
+    if(amt_value === "" || amt_value < 1) {
+        amount.value = 1;
         amt_value = "1";
     }
 
-    let url = `${BASE_URL}/${from_select.value.toLowerCase()}.json`;
-
-    let fetching_data = await fetch(url)
+    let url = `${BASE_URL}/${from_select.value.toLowerCase()}.json`
+    let fetching_data = await fetch(url);
     let data = await fetching_data.json();
-    console.log(data);
     let rate = data[from_select.value.toLowerCase()][to_select.value.toLowerCase()];
-    console.log(rate);
 
     let total_amount = amt_value * rate;
-    display.innerText=`${amt_value} ${from_select.value} = ${total_amount} ${to_select.value}`;
+    display.innerText =`${amt_value} ${from_select.value} = ${total_amount} ${to_select.value}`;
 }
 
 btn.addEventListener('click' , (e) => {
@@ -62,6 +59,7 @@ from_select.addEventListener('change' , (e) => {
     e.target.addEventListener('change', (e) => {
         co.classList.add('.flag')
     })
+    update_rate();
 })
 
 
@@ -70,19 +68,20 @@ to_select.addEventListener('change' , (e) => {
     e.target.addEventListener('change', (e) => {
         co.classList.add('.flag')
     })
+    update_rate();
 })
 
-update_flag = (eve) => {
-    let code = country_list[eve.value];
+update_flag = (e) => {
 
-    let new_src = `https://flagsapi.com/${code}/flat/64.png`
-
-    let img = eve.parentElement.querySelector('img')
-
-    img.src = new_src;
+    let Coun_code = country_list[e.value];
+    let new_src = `https://flagsapi.com/${Coun_code}/flat/64.png`
+    let img=e.parentElement.querySelector('img');
+    img.src=new_src;
+    
 }
 
 let ex=document.querySelector(".selection i");
+
 ex.addEventListener("click",()=>{
     let t1=from_select.value;
     let t2=to_select.value;
